3de9f7c2d62625821a990ec369a51396d1e153bc
VergilZh <z1205067147@gmail.com>
2020-12-10 20:41:06 -0500

    Add moving platform

---
+++ Assets/Scripts/MovingPlatformControlller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformControlller : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+
+    private Vector3 distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = end.position - start.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+    }
+
+    private void _Move()
+    {
+        transform.position = new Vector3(start.position.x + Mathf.PingPong(Time.time, distance.x), start.position.y, 0.0f);
+    }
+}
