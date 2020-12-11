fd6a37b8ab21f4fbd3561e62da652d05526c8153
VergilZh <z1205067147@gmail.com>
2020-12-10 20:36:53 -0500

    Add button management

---
+++ Assets/Scripts/ButtonManagement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMainMenuButtonPressed()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnInstructionButtonPressed()
+    {
+        SceneManager.LoadScene("Instruction");
+    }
+
+    public void OnGameOverButtonPressed()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void OnStartButtonPressed()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
