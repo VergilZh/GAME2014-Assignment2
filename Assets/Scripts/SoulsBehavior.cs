e4afef14d1ad7013fb0604243f8079407b699691
VergilZh <z1205067147@gmail.com>
2020-12-10 20:35:54 -0500

    Add souls player can pick up

---
+++ Assets/Scripts/SoulsBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulsBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if(other.gameObject.GetComponent<PlayerBehaviour>().playerHealth < 4)
+            {
+                other.gameObject.GetComponent<PlayerBehaviour>().playerHealth += 1;
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
